688f5331121c870ef6e9e36462cdb214082aa0a5
Amit Kumar <aktech@users.noreply.github.com>
2026-06-15 15:19:48 +0100

    test: assert quickstart title renders in h1 element

--- tests/test_smoke.sh
+++ tests/test_smoke.sh
@@ -3,5 +3,5 @@ source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 build_site
 assert_file "$PUB/index.html" "home page builds"
 assert_file "$PUB/docs/quickstart/index.html" "doc page builds"
-assert_grep "$PUB/docs/quickstart/index.html" "Quickstart" "doc renders its title"
+assert_grep "$PUB/docs/quickstart/index.html" "<h1>Quickstart</h1>" "doc renders its title in h1"
 finish
